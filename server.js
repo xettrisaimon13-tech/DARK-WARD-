fafe51964f88bfaeaca0479858d718a5e9e6c2ac
xettrisaimon13-tech <xettrisaimon13@gmail.com>
2026-07-04 02:35:33 -0700

    Refactor server.js to simplify functionality
    Removed CORS middleware and several endpoints related to room hosting and joining. Added a simple root endpoint and updated the server to listen on a configurable port.

--- server.js
+++ server.js
@@ -1,56 +1,13 @@
 const express = require("express");
-const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+const PORT = process.env.PORT || 3000;
 
-const rooms = {};
-
-app.post("/host", (req, res) => {
-
-    const roomId = Math.floor(100000 + Math.random() * 900000).toString();
-
-    rooms[roomId] = {
-        host_ip: req.body.ip,
-        players: 1
-    };
-
-    res.json({
-        success: true,
-        room: roomId
-    });
-
+app.get("/", (req, res) => {
+    res.send("Dark Ward Server Running");
 });
 
-app.post("/join", (req, res) => {
-
-    const room = rooms[req.body.room];
-
-    if (!room) {
-        return res.json({
-            success: false
-        });
-    }
-
-    room.players++;
-
-    res.json({
-        success: true,
-        ip: room.host_ip
-    });
-
-});
-
-app.get("/rooms", (req, res) => {
-
-    res.json(rooms);
-
-});
-
-app.listen(3000, () => {
-
-    console.log("Server Running");
-
+app.listen(PORT, () => {
+    console.log("Server Running on", PORT);
 });
